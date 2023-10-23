5eb75e91427fe564a1555edb096947f40a8bea5d
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-10-23 22:14:32 +0200

    initial commit

---
+++ src/main/java/cz/cvut/ear/DarkstoreApi/DarkstoreApiApplication.java
@@ -0,0 +1,12 @@
+package cz.cvut.ear.DarkstoreApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkstoreApiApplication.class, args);
+	}
+}
---
+++ src/test/java/cz/cvut/ear/DarkstoreApi/DarkstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.ear.DarkstoreApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkstoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
