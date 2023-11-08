c42bdc6d40e45b8bb611878ee057b2e45dfbe475
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-11-08 21:28:47 +0100

    impl of createOrders method

--- src/main/java/cz/cvut/ear/DarkstoreApi/service/OrderServiceImpl.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/service/OrderServiceImpl.java
@@ -1,12 +1,16 @@
 package cz.cvut.ear.DarkstoreApi.service;
 
+import cz.cvut.ear.DarkstoreApi.dto.CreateOrderDto;
 import cz.cvut.ear.DarkstoreApi.dto.CreateOrderRequest;
 import cz.cvut.ear.DarkstoreApi.dto.OrderDto;
+import cz.cvut.ear.DarkstoreApi.model.order.Order;
 import cz.cvut.ear.DarkstoreApi.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -14,10 +18,23 @@ import java.util.List;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public List<OrderDto> createOrders(CreateOrderRequest createOrderRequest) {
-        return null;
+        List<CreateOrderDto> createOrderDtos = createOrderRequest.getOrders();
+
+        List<Order> savedOrders = createOrderDtos.stream()
+                .map(createOrderDto -> modelMapper.map(createOrderDto, Order.class))
+                .collect(Collectors.toList());
+
+        savedOrders = orderRepository.saveAll(savedOrders);
+
+        List<OrderDto> orderDtos = savedOrders.stream()
+                .map(order -> modelMapper.map(order, OrderDto.class))
+                .collect(Collectors.toList());
+
+        return orderDtos;
     }
 
     @Override
