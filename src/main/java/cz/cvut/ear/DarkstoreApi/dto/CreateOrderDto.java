a40485a9004438765140ba4708ee72f813ece70c
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-11-12 17:45:38 +0100

    add JsonProperty to deliveryHours

--- src/main/java/cz/cvut/ear/DarkstoreApi/dto/CreateOrderDto.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/dto/CreateOrderDto.java
@@ -1,5 +1,6 @@
 package cz.cvut.ear.DarkstoreApi.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
@@ -16,6 +17,7 @@ public class CreateOrderDto {
     @Positive
     private int region;
 
+    @JsonProperty("delivery_hours")
     @Valid
     private List<@Pattern(regexp = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]-(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$") String> deliveryHours;
 
