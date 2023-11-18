cdcda3bec26b0d4325deaa47027594622a11e1e4
bahmetm <bagmet.maxim@gmail.com>
2023-11-18 19:23:09 +0100

    fix validation and add validation messages to CreateOrderDto

--- src/main/java/cz/cvut/ear/DarkstoreApi/dto/CreateOrderDto.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/dto/CreateOrderDto.java
@@ -19,7 +19,8 @@ public class CreateOrderDto {
 
     @JsonProperty("delivery_hours")
     @Valid
-    private List<@Pattern(regexp = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]-(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$") String> deliveryHours;
+    private List<@Pattern(regexp = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]-(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$",
+            message = "Provided invalid delivery_hours.") String> deliveryHours;
 
     @Positive
     private int cost;
