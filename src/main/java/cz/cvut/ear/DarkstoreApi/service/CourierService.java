4e340c247131d7fa801f2d830bd6c92f75cb6090
bahmetm <bagmet.maxim@gmail.com>
2023-11-18 19:28:48 +0100

    remove service interfaces

--- src/main/java/cz/cvut/ear/DarkstoreApi/service/CourierService.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/service/CourierService.java
@@ -1,15 +1,46 @@
 package cz.cvut.ear.DarkstoreApi.service;
 
-import cz.cvut.ear.DarkstoreApi.dto.*;
+import cz.cvut.ear.DarkstoreApi.dto.CourierDto;
+import cz.cvut.ear.DarkstoreApi.dto.CreateCourierRequest;
+import cz.cvut.ear.DarkstoreApi.model.Role;
+import cz.cvut.ear.DarkstoreApi.model.courier.Courier;
+import cz.cvut.ear.DarkstoreApi.repository.CourierRepository;
+import cz.cvut.ear.DarkstoreApi.util.mapper.CourierMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface CourierService {
 
-    ResponseEntity<List<CourierDto>> createCouriers(CreateCourierRequest createCourierRequest);
+@Service
+@RequiredArgsConstructor
+public class CourierService {
+    private final CourierRepository courierRepository;
+    private final CourierMapper courierMapper;
 
-    ResponseEntity<List<CourierDto>> getCouriers(int limit, int offset);
+    public ResponseEntity<List<CourierDto>> createCouriers(CreateCourierRequest createCourierRequest) {
+        List<Courier> savedCouriers = courierMapper.createCourierDtoToCourier(createCourierRequest.getCouriers());
 
-    ResponseEntity<CourierDto> getCourier(long courierId);
+        savedCouriers.forEach(courier -> courier.setRole(Role.ROLE_COURIER));
+
+        savedCouriers = courierRepository.saveAll(savedCouriers);
+
+        List<CourierDto> courierDtos = courierMapper.courierToCourierDto(savedCouriers);
+
+        return ResponseEntity.ok(courierDtos);
+    }
+
+    public ResponseEntity<List<CourierDto>> getCouriers(int limit, int offset) {
+        return ResponseEntity.ok(courierMapper.courierToCourierDto(courierRepository.findAll().stream()
+                .skip(offset)
+                .limit(limit)
+                .toList()));
+    }
+
+    public ResponseEntity<CourierDto> getCourier(long courierId) {
+        return courierRepository.findById(courierId)
+                .map(courier -> ResponseEntity.ok(courierMapper.courierToCourierDto(courier)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/cz/cvut/ear/DarkstoreApi/service/OrderService.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/service/OrderService.java
@@ -1,47 +1,113 @@
 package cz.cvut.ear.DarkstoreApi.service;
 
-import cz.cvut.ear.DarkstoreApi.dto.CompleteOrderRequestDto;
-import cz.cvut.ear.DarkstoreApi.dto.CreateOrderRequest;
-import cz.cvut.ear.DarkstoreApi.dto.OrderDto;
+import cz.cvut.ear.DarkstoreApi.dto.*;
+import cz.cvut.ear.DarkstoreApi.model.order.Order;
+import cz.cvut.ear.DarkstoreApi.model.order.OrderStatus;
+import cz.cvut.ear.DarkstoreApi.repository.OrderRepository;
+import cz.cvut.ear.DarkstoreApi.util.mapper.OrderMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
-
-public interface OrderService {
-
-    /**
-     * Accepts a list of order data in JSON format and creates orders. Order characteristics are displayed - weight, region, delivery time and price.
-     * Delivery time is a string in the format HH:MM-HH:MM, where HH - hours (from 0 to 23) and MM - minutes (from 0 to 59).
-     *
-     * @param createOrderRequest The request containing a list of order data in JSON format.
-     * @return A list of OrderDto objects representing the created orders.
-     */
-    ResponseEntity<List<OrderDto>> createOrders(CreateOrderRequest createOrderRequest);
-
-    /**
-     * Retrieves a list of orders with optional pagination.
-     * If offset or limit are not passed, the default is offset = 0, limit = 1.
-     *
-     * @param limit The maximum number of orders to retrieve.
-     * @param offset The offset for pagination.
-     * @return A list of OrderDto objects representing the retrieved orders.
-     */
-    ResponseEntity<List<OrderDto>> getOrders(int limit, int offset);
-
-    /**
-     * Retrieves details of a specific order by ID.
-     *
-     * @param orderId ID of the order to retrieve.
-     * @return An OrderDto object representing the retrieved order.
-     */
-    ResponseEntity<OrderDto> getOrder(long orderId);
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
-    /**
-     * Completes multiple orders at once.
-     *
-     * @param completeOrderRequestDto The CompleteOrderRequestDto containing information about the orders to complete.
-     * @return list of OrderDto representing the completed orders.
-     */
-    ResponseEntity<List<OrderDto>> completeOrders(CompleteOrderRequestDto completeOrderRequestDto);
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+
+    public ResponseEntity<List<OrderDto>> createOrders(CreateOrderRequest createOrderRequest) {
+        List<CreateOrderDto> createOrderDtos = createOrderRequest.getOrders();
+
+        List<Order> savedOrders = orderMapper.createOrderDtoToOrder(createOrderDtos);
+
+        savedOrders.forEach(order -> order.setStatus(OrderStatus.CREATED));
+
+        savedOrders = orderRepository.saveAll(savedOrders);
+
+        if (savedOrders.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        List<OrderDto> orderDtos = orderMapper.orderToOrderDto(savedOrders);
+
+        return ResponseEntity.ok(orderDtos);
+    }
+
+    public ResponseEntity<List<OrderDto>> getOrders(int limit, int offset) {
+        List<OrderDto> orderDtos = orderMapper.orderToOrderDto(orderRepository.findAll().stream()
+                .skip(offset)
+                .limit(limit)
+                .toList());
+
+        if (orderDtos.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        return ResponseEntity.ok(orderDtos);
+    }
+
+    public ResponseEntity<OrderDto> getOrder(long orderId) {
+        Optional<Order> order = orderRepository.findById(orderId);
+
+        if (order.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        OrderDto orderDto = orderMapper.orderToOrderDto(order.get());
+        return ResponseEntity.ok(orderDto);
+    }
+
+    public ResponseEntity<List<OrderDto>> completeOrders(CompleteOrderRequestDto completeOrderRequestDto) {
+        List<Order> completedOrders = new ArrayList<>();
+        List<CompleteOrder> completeOrders = completeOrderRequestDto.getCompleteOrders();
+
+//        List<Order> ordersToComplete = completeOrders.stream()
+//                .map(completeOrder -> orderRepository.findById(completeOrder.getOrderId()))
+//                .filter(Optional::isPresent)
+//                .map(Optional::get)
+//                .toList();
+
+
+
+        for(CompleteOrder completeOrder : completeOrders) {
+            Optional<Order> order = orderRepository.findById(completeOrder.getOrderId());
+
+            if(order.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+
+//            if (!isOrderValid(order.get(), completeOrder)) {
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//            }
+
+            order.get().setCompleteTime(LocalDateTime.parse(completeOrder.getCompleteTime(), DateTimeFormatter.ISO_DATE_TIME));
+            order.get().setStatus(OrderStatus.FINISHED);
+            completedOrders.add(order.get());
+        }
+
+        orderRepository.saveAll(completedOrders);
+
+        List<OrderDto> orderDtos = completedOrders.stream()
+                .map(orderMapper::orderToOrderDto)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(orderDtos);
+    }
+
+    private boolean isOrderValid(Order order, CompleteOrder completeOrder) {
+        if (order.getStatus() != OrderStatus.ASSIGNED/** || order.getCourier().getId() != completeOrder.getCourierId()*/) {
+            return false;
+        }
+        return true;
+    }
 }
