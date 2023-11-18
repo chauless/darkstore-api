3f00d5be8a84769a19427e106e68f94bd52fd344
bahmetm <bagmet.maxim@gmail.com>
2023-11-18 19:22:55 +0100

    fix validation and add validation messages to CreateCourierDto

--- src/main/java/cz/cvut/ear/DarkstoreApi/dto/CreateCourierDto.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/dto/CreateCourierDto.java
@@ -1,11 +1,9 @@
 package cz.cvut.ear.DarkstoreApi.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import cz.cvut.ear.DarkstoreApi.model.courier.CourierType;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.util.List;
@@ -13,18 +11,20 @@ import java.util.List;
 @Data
 public class CreateCourierDto {
 
-    @NotNull
+    @NotNull(message = "Provided invalid type.")
     private CourierType type;
 
-    @NotEmpty
+    @NotNull(message = "Provided invalid regions.")
     private List<Integer> regions;
 
+    @JsonProperty("working_hours")
     @Valid
-    private List<@Pattern(regexp = "([0-1]?[0-9]|2[0-3]):[0-5][0-9]-([0-1]?[0-9]|2[0-3]):[0-5][0-9]") String> workingHour;
+    private List<@Pattern(regexp = "([0-1]?[0-9]|2[0-3]):[0-5][0-9]-([0-1]?[0-9]|2[0-3]):[0-5][0-9]",
+    message = "Provided invalid working_hours.") String> workingHours;
 
     @Email
     private String email;
 
-    @NotNull
+    @NotBlank
     private String password;
 }
