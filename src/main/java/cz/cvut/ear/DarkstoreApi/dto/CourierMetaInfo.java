515331ca7aea4efce66969512a015d6d4a06af49
bahmetm <bagmet.maxim@gmail.com>
2023-11-19 16:10:11 +0100

    add MetaInfo dto

---
+++ src/main/java/cz/cvut/ear/DarkstoreApi/dto/CourierMetaInfo.java
@@ -0,0 +1,11 @@
+package cz.cvut.ear.DarkstoreApi.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class CourierMetaInfo {
+    private int earnings;
+    private double rate;
+}
