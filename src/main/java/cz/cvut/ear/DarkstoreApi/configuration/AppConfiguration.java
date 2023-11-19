3406da1236b9bd6c50bc4fead2f6c64873733dda
bahmetm <bagmet.maxim@gmail.com>
2023-11-19 18:37:37 +0100

    add RateLimitingFilter configuration

--- src/main/java/cz/cvut/ear/DarkstoreApi/configuration/AppConfiguration.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/configuration/AppConfiguration.java
@@ -1,8 +1,17 @@
 package cz.cvut.ear.DarkstoreApi.configuration;
 
+import cz.cvut.ear.DarkstoreApi.controller.filter.RateLimitingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfiguration {
-
+    @Bean
+    public FilterRegistrationBean<RateLimitingFilter> rateLimitingFilterRegistration(RateLimitingFilter rateLimitingFilter) {
+        FilterRegistrationBean<RateLimitingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(rateLimitingFilter);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
 }
