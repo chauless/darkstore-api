6f1d4c1e13da141ea0c4158d20a61239bd82d859
bahmetm <bagmet.maxim@gmail.com>
2023-12-01 21:40:50 +0100

    update dtos and mappers to match changed models

---
+++ src/main/java/cz/cvut/ear/DarkstoreApi/dto/OrderGroupDto.java
@@ -0,0 +1,16 @@
+package cz.cvut.ear.DarkstoreApi.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class OrderGroupDto {
+    private long id;
+
+    @JsonProperty("courier_id")
+    private long courierId;
+
+    private List<OrderDto> orders;
+}
--- src/main/java/cz/cvut/ear/DarkstoreApi/util/mapper/OrderMapper.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/util/mapper/OrderMapper.java
@@ -14,39 +14,49 @@ import java.util.stream.Collectors;
 public interface OrderMapper {
     OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
 
-    @Named("convertToDeliveryHours")
-    static List<DeliveryHour> convertToDeliveryHours(List<String> deliveryHours) {
-        return deliveryHours.stream().map(deliveryHour -> {
-            String[] hours = deliveryHour.split("-");
-            return new DeliveryHour(hours[0], hours[1]);
-        }).collect(Collectors.toList());
+    @Named("convertToDeliveryHour")
+    static DeliveryHour convertToDeliveryHour(String deliveryHour) {
+        String[] hours = deliveryHour.split("-");
+        return new DeliveryHour(hours[0], hours[1]);
     }
 
-    @Named("deliveryHoursToString")
-    static List<String> deliveryHoursToString(List<DeliveryHour> deliveryHours) {
-        return deliveryHours.stream().map(deliveryHour -> deliveryHour.getStart()
-                + "-" + deliveryHour.getFinish()).collect(Collectors.toList());
+    @Named("deliveryHourToString")
+    static String deliveryHourToString(DeliveryHour deliveryHour) {
+        return deliveryHour.getStart() + "-" + deliveryHour.getFinish();
     }
 
-    @Mapping(target = "id", ignore = true)
+//    @Mapping(target = "id", ignore = true)
+//    @Mapping(target = "orderGroup", ignore = true)
     @Mapping(source = "weight", target = "weight")
     @Mapping(source = "region", target = "region")
-    @Mapping(source = "deliveryHours", target = "deliveryHours", qualifiedByName = "convertToDeliveryHours")
+    @Mapping(source = "deliveryHour", target = "deliveryHour", qualifiedByName = "convertToDeliveryHour")
     @Mapping(source = "cost", target = "cost")
     Order createOrderDtoToOrder(CreateOrderDto createOrderDto);
 
+//    default List<String> map(List<DeliveryHour> value) {
+//        return value.stream().map(deliveryHour -> deliveryHour.getStart()
+//                + "-" + deliveryHour.getFinish()).collect(Collectors.toList());
+//    }
+
     @AfterMapping
     default void setOrder(@MappingTarget Order order) {
-        order.getDeliveryHours().forEach(deliveryHour -> deliveryHour.setOrder(order));
+//        order.getDeliveryHour().forEach(deliveryHour -> deliveryHour.setOrder(order));
+        order.getDeliveryHour().setOrder(order);
     }
 
-    @Mapping(source = "id", target = "orderId")
+    @Named("idToOrderId")
+    static Long idToOrderId(long id) {
+        return id;
+    }
+
+    @Mapping(source = "id", target = "orderId", qualifiedByName = "idToOrderId")
     @Mapping(source = "weight", target = "weight")
     @Mapping(source = "region", target = "region")
-    @Mapping(source = "deliveryHours", target = "deliveryHours", qualifiedByName = "deliveryHoursToString")
+    @Mapping(source = "deliveryHour", target = "deliveryHour", qualifiedByName = "deliveryHourToString")
     @Mapping(source = "cost", target = "cost")
     @Mapping(source = "completeTime", target = "completeTime")
     OrderDto orderToOrderDto(Order order);
+
 
     List<Order> createOrderDtoToOrder(List<CreateOrderDto> createOrderDto);
 
