79ab266679f994da4d919b83051dae8c290fe616
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-12-03 12:13:43 +0100

    add CourierSummaryDto

---
+++ src/main/java/cz/cvut/ear/DarkstoreApi/dto/CourierSummaryDto.java
@@ -0,0 +1,9 @@
+package cz.cvut.ear.DarkstoreApi.dto;
+
+import cz.cvut.ear.DarkstoreApi.model.courier.CourierType;
+
+public class CourierSummaryDto {
+
+    private Long id;
+    private CourierType type;
+}
