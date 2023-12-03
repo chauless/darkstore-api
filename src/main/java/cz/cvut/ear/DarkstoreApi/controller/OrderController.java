eb08d2ba480446e7f0d46f872dad5f318f0b99b0
bahmetm <bagmet.maxim@gmail.com>
2023-12-03 20:24:36 +0100

    add security to order endpoints

--- src/main/java/cz/cvut/ear/DarkstoreApi/controller/OrderController.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/controller/OrderController.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +24,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
     @PostMapping
     public ResponseEntity<List<OrderDto>> createOrders(@RequestBody @Valid CreateOrderRequest createOrderRequest) {
         return ResponseEntity.ok(orderService.createOrders(createOrderRequest));
@@ -29,20 +32,24 @@ public class OrderController {
 
     @GetMapping
     public ResponseEntity<List<OrderDto>> getOrders(@RequestParam(required = false, defaultValue = "1") @PositiveOrZero int limit,
-                                                    @RequestParam(required = false, defaultValue = "0") @PositiveOrZero int offset) {
-        return ResponseEntity.ok(orderService.getOrders(limit, offset));
+                                                    @RequestParam(required = false, defaultValue = "0") @PositiveOrZero int offset,
+                                                    Authentication authentication) {
+        return ResponseEntity.ok(orderService.getOrders(authentication, limit, offset));
     }
 
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
     @GetMapping("/{order_id}")
     public ResponseEntity<OrderDto> getOrderById(@PathVariable(name = "order_id") long orderId) {
         return ResponseEntity.ok(orderService.getOrder(orderId));
     }
 
+    @PreAuthorize("hasRole('ROLE_COURIER')")
     @PostMapping("/complete")
     public ResponseEntity<List<OrderDto>> completeOrders(@RequestBody @Valid CompleteOrderRequestDto completeOrderRequestDto) {
         return ResponseEntity.ok(orderService.completeOrders(completeOrderRequestDto));
     }
 
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
     @PostMapping("/assign")
     public ResponseEntity<List<OrderGroupDto>> assignOrders() {
         orderService.deleteAllOrderGroups();
