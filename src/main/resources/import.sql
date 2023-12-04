b034a05584ccaf61192e20c6b3ef94cfb4b6dda3
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-12-04 21:39:24 +0100

    add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (email, password) VALUES ('manager@gmail.com', '$2a$12$Wm5GvU1TUysybx63gdL7eObi10auk6ze3xGc4iz.DsBOKReFHaDiy'); -- password: 12345
+INSERT INTO managers (id) VALUES (1);
