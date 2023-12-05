aa3fa8bbe149166d9a94e4b35df4e8a5648a35c4
bahmetm <bagmet.maxim@gmail.com>
2023-12-05 16:29:26 +0100

    add validation exception handling

--- src/main/java/cz/cvut/ear/DarkstoreApi/controller/GlobalExceptionHandler.java
+++ src/main/java/cz/cvut/ear/DarkstoreApi/controller/GlobalExceptionHandler.java
@@ -6,10 +6,14 @@ import cz.cvut.ear.DarkstoreApi.exception.OrderNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,6 +26,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({DataIntegrityViolationException.class, IllegalArgumentException.class})
     public ResponseEntity<?> handleDataIntegrityViolationException(RuntimeException ex) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(FieldError::getDefaultMessage)
+                .orElse("Validation error");
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), message);
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
